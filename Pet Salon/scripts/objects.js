d360f60c789ce477b94c0cbfb5e8043b1adba6c0
Reginald W Grant Jr <grantreggie09@gmail.com>
2025-08-03 13:34:12 -0700

    Add files via upload

---
+++ Pet Salon/scripts/objects.js
@@ -0,0 +1,30 @@
+let pet1 = {
+    
+    name: "Lexi",
+    age: 1,
+    gender: "female",
+    service: "Basic Wash",
+    breed: "French Bulldog",
+
+};
+
+let pet2 = {
+    
+    name: "Domino",
+    age: 6,
+    gender: "male",
+    service: "Deluxe Detail",
+    breed: "Great Dane",
+    
+};
+
+let pet3 = {
+
+    name: "Otis",
+    age: 17,
+    gender: "male",
+    service: "Super Deluxe Detail",
+    breed: "Chihuahua",
+};
+
+
